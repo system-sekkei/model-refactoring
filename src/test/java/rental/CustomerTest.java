dfdfcb725fb2dbd9d9e253f20d1cdf426ea2f818
masuda220 <masuda220@gmail.com>
2018-09-03 13:11:05 +0900

    junit 4 -> 5

--- src/test/java/rental/CustomerTest.java
+++ src/test/java/rental/CustomerTest.java
@@ -1,12 +1,13 @@
 package rental;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class CustomerTest {
 
     static final Movie newRelease = new Movie("新作", Movie.NEW_RELEASE);
     static final Movie children = new Movie("子供", Movie.CHILDREN);
     static final Movie regular = new Movie("一般", Movie.REGULAR);
+
     @Test
     public void smokeTest() {
         Customer customer = new Customer("山田") ;

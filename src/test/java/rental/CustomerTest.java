1e1736497fa81202b23f5826fc9fe7df63b8b193
masuda220 <masuda220@gmail.com>
2018-09-03 13:02:55 +0900

    gradle 4.0 -> 4.9, テスト　変数名 newOne -> newRelease

--- src/test/java/rental/CustomerTest.java
+++ src/test/java/rental/CustomerTest.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 
 public class CustomerTest {
 
-    static final Movie newOne = new Movie("新作", Movie.NEW_RELEASE);
+    static final Movie newRelease = new Movie("新作", Movie.NEW_RELEASE);
     static final Movie children = new Movie("子供", Movie.CHILDREN);
     static final Movie regular = new Movie("一般", Movie.REGULAR);
     @Test
     public void smokeTest() {
         Customer customer = new Customer("山田") ;
-        customer.addRental(new Rental(newOne,3));
+        customer.addRental(new Rental(newRelease,3));
         customer.addRental(new Rental(children,2));
         customer.addRental(new Rental(regular,1));
         System.out.println(customer.statement());
